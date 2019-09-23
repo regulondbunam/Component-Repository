8e503f0e2e2b14f808cd00edc18c94fb6bc10780
Gabriel CCG <galarcon@ccg.unam.mx>
2019-09-23 11:00:31 -0500

    npm init and principal configuration

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+const W = 500
+const H = 500
+
+export class Hola extends Component {
+  render() {
+    return (
+      <div >
+        <h1>Hola</h1>
+      </div>
+    )
+  }
+}
