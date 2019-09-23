9be77659eb38ce410cb115e982b53bbd6929666a
Gabriel CCG <galarcon@ccg.unam.mx>
2019-09-23 14:19:46 -0500

    update doc button and create props in code button

--- doc/button.md
+++ doc/button.md
@@ -13,7 +13,7 @@ name | Data Type  | Default  | Description
 children  | node  |   | The content of the button
 disabled  | boolean  | false  | disabled button
 size  | string   | 'default'  | size of button
-look  | object  | ---  | Style of Button
+look  | object  | {}  | Style of Button
 ripples | boolean | true | enable or disabled button animation
 onClick  | function(event)  | noAction()  | on click event function
-to  | String  | '' | url   
+to  | String  | 'noURL' | url
--- src/basicInput/buttons/Button.js
+++ src/basicInput/buttons/Button.js
@@ -1,6 +1,44 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 import './Style.css'
 
 export default class Button extends Component {
+    static propTypes = {
+        disabled: Boolean,
+        size: String,
+        look: Object,
+        ripples: Boolean,
+        onClick: Function,
+        to: String,
+    }
+
+    handleOnClickLink = (event) => {
+        this.props.onClick(event);
+    }
     
+    render() {
+        return (
+            <button
+                type="button"
+                className="simpleButton"
+                onClick={handleOnClickLink}
+            >
+                {props.children}
+            </button>
+        )
+    }
+
+}
+
+Button.defaultProps = {
+    disabled: false,
+    size: 'default',
+    look: {},
+    ripples: true,
+    onClick: noAction(),
+    to: 'noURL'
+}
+
+function noAction(){
+    console.log("no action defined")
 }
