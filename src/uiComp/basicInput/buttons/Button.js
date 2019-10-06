a9a97cfa2238e64b1b908ddc80c5fd18964afbf4
Galarcon128 <alarcongabriel128@gmail.com>
2019-10-06 16:15:47 -0500

    update Button component 0.0.22

--- src/uiComp/basicInput/buttons/Button.js
+++ src/uiComp/basicInput/buttons/Button.js
@@ -1,44 +1,62 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import './Style.css'
-
+import {STYLES_BUTTON} from './StaticVar'
+ 
 export default class Button extends Component {
-    static propTypes = {
-        disabled: Boolean,
-        size: String,
-        look: Object,
-        ripples: Boolean,
-        onClick: Function,
-        to: String,
-    }
 
-    handleOnClickLink = (event) => {
-        this.props.onClick(event);
-    }
     
+ 
+    handleOnClickLink = (event) => {
+            this.props.onClick(event);
+    }
+     
     render() {
+        let style = STYLES_BUTTON[0];
+
+        if(this.props.look !== {}){
+            switch(this.props.type){
+                case 'accent':
+                    style = STYLES_BUTTON[1];
+                    break;
+                default:
+                    style = STYLES_BUTTON[0];
+            }
+        }
         return (
             <button
                 type="button"
-                className="simpleButton"
+                className={style}
                 onClick={this.handleOnClickLink}
             >
                 {this.props.children}
             </button>
         )
     }
-
+ 
 }
 
+Button.propTypes = {
+    disabled: PropTypes.bool,
+    size: PropTypes.string,
+    type: PropTypes.string,
+    look: PropTypes.object,
+    ripples: PropTypes.bool,
+    onClick: PropTypes.func,
+    href: PropTypes.string
+}
+ 
 Button.defaultProps = {
+    children: "RegulonDB Button!!",
     disabled: false,
     size: 'default',
+    type: 'default',
     look: {},
     ripples: true,
-    onClick: noAction(),
-    to: 'noURL'
+    href: null,
+    onClick: noAction
 }
 
-function noAction(){
-    console.log("no action defined")
-}
+function noAction (){
+    console.error("Button has no activity")
+} 
---
+++ src/uiComp/basicInput/buttons/StaticVar.js
@@ -0,0 +1,4 @@
+export let STYLES_BUTTON = [
+    'simpleButton',
+    'accentButton'
+];
