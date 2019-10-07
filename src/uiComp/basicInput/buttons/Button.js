7c780b262a28f3fdd21292185cb38ffb1b64185c
Galarcon128 <alarcongabriel128@gmail.com>
2019-10-06 23:14:16 -0500

    add Disabled Button function

--- src/uiComp/basicInput/buttons/Button.js
+++ src/uiComp/basicInput/buttons/Button.js
@@ -1,62 +1,60 @@
-import React, { Component } from 'react'
-import PropTypes from 'prop-types'
-import './Style.css'
-import {STYLES_BUTTON} from './StaticVar'
- 
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import './Style.css';
+import { STYLES_BUTTON } from './StaticVar';
+
 export default class Button extends Component {
+	handleOnClickLink = (event) => {
+        this.props.disabled
+        ?noAction()
+        :this.props.onClick(event);
+	};
 
-    
- 
-    handleOnClickLink = (event) => {
-            this.props.onClick(event);
-    }
-     
-    render() {
-        let style = STYLES_BUTTON[0];
+	render() {
+		let style = STYLES_BUTTON[1];
 
-        if(this.props.look !== {}){
-            switch(this.props.type){
-                case 'accent':
-                    style = STYLES_BUTTON[1];
-                    break;
-                default:
-                    style = STYLES_BUTTON[0];
+		if (this.props.look !== {}) {
+			if (!this.props.disabled) {
+				switch (this.props.type) {
+					case 'accent':
+						style = STYLES_BUTTON[2];
+						break;
+					default:
+						style = STYLES_BUTTON[1];
+				}
+			}else{
+                style = STYLES_BUTTON[0];
             }
-        }
-        return (
-            <button
-                type="button"
-                className={style}
-                onClick={this.handleOnClickLink}
-            >
-                {this.props.children}
-            </button>
-        )
-    }
- 
+		}
+		return (
+			<button type="button" className={style} onClick={this.handleOnClickLink}>
+				{this.props.children}
+			</button>
+		);
+	}
 }
 
 Button.propTypes = {
-    disabled: PropTypes.bool,
-    size: PropTypes.string,
-    type: PropTypes.string,
-    look: PropTypes.object,
-    ripples: PropTypes.bool,
-    onClick: PropTypes.func,
-    href: PropTypes.string
-}
- 
-Button.defaultProps = {
-    children: "RegulonDB Button!!",
-    disabled: false,
-    size: 'default',
-    type: 'default',
-    look: {},
-    ripples: true,
-    href: null,
-    onClick: noAction
-}
+	disabled: PropTypes.bool,
+	size: PropTypes.string,
+	type: PropTypes.string,
+	look: PropTypes.object,
+	ripples: PropTypes.bool,
+	onClick: PropTypes.func,
+	href: PropTypes.string
+};
 
-function noAction (){
-    console.error("Button has no activity")
-} 
+Button.defaultProps = {
+	children: 'RegulonDB Button!!',
+	disabled: false,
+	size: 'default',
+	type: 'default',
+	look: {},
+	ripples: true,
+	href: null,
+	onClick: noAction
+};
+
+function noAction() {
+	console.error('Button has no activity');
+}
--- src/uiComp/basicInput/buttons/StaticVar.js
+++ src/uiComp/basicInput/buttons/StaticVar.js
@@ -1,4 +1,5 @@
 export let STYLES_BUTTON = [
+    'disableButton',
     'simpleButton',
     'accentButton'
 ];
