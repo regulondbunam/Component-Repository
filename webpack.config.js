9978e0ccaa20e93fc3fe731684d03044e78987a6
Galarcon128 <alarcongabriel128@gmail.com>
2019-10-06 23:37:24 -0500

    css add suport in webpack build

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,29 @@
 var path = require('path');
 
-module.exports = [{
-    mode: 'production',
-    entry: './src/uiComp/basicInput/buttons/Button.js',
-    output: {
-        path: path.resolve('uiComp/basicInput/buttons'),
-        filename: 'Button.js',
-        libraryTarget: 'commonjs2'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.jsx?$/,
-                exclude: /(node_modules)/,
-                use: 'babel-loader'
-            }
-        ]
-    }
-}]
+module.exports = [
+	{
+		mode: 'production',
+		entry: './src/uiComp/basicInput/buttons/Button.js',
+		output: {
+			path: path.resolve('uiComp/basicInput/buttons'),
+			filename: 'Button.js',
+			libraryTarget: 'commonjs2'
+		},
+		module: {
+			rules: [
+				{
+					test: /\.jsx?$/,
+					exclude: /(node_modules)/,
+					use: 'babel-loader'
+				},
+				{
+					test: /\.(s*)css$/,
+                    use: [ 
+                        'style-loader',
+                        'css-loader',
+                    ]
+				}
+			]
+		}
+	}
+];
