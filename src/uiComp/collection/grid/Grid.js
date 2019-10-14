d32e21970a2b57dc02a1dc2a257c2d2d49c6fda4
Gabriel CCG <galarcon@ccg.unam.mx>
2019-10-14 13:53:23 -0500

    init grid

---
+++ src/uiComp/collection/grid/Grid.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom'
+import PropTypes from 'prop-types';
+import './Style.css';
+
+export default class Grid extends Component {
+    
+}
