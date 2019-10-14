c946c13bf4f9a419b5defa0c1742c76a99b01617
Gabriel CCG <galarcon@ccg.unam.mx>
2019-10-14 14:55:18 -0500

    grid style on simple and accent

--- src/uiComp/collection/grid/Grid.js
+++ src/uiComp/collection/grid/Grid.js
@@ -4,5 +4,12 @@ import PropTypes from 'prop-types';
 import './Style.css';
 
 export default class Grid extends Component {
-    
+    render() {
+        return (
+            <div>
+
+            </div>
+        )
+
+    }
 }
