ff4c7a420f542781c4aa633f59bfdd618520e13c
Gabriel CCG <galarcon@ccg.unam.mx>
2019-11-12 12:42:07 -0600

    add Buttons

---
+++ src/uiComp/basicInput/Buttons.js
@@ -0,0 +1,5 @@
+import Button, {CircularB} from './buttons/Button'
+import DropDown from './buttons/DropDown'
+import Flip from './buttons/Flip'
+import Split from './buttons/Split'
+export {Button, DropDown, CircularB, Flip, Split}
