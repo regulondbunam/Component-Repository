30ed34c8f8e9f32c83fc5e5172df6d84b26193e3
Galarcon128 <alarcongabriel128@gmail.com>
2020-03-25 16:09:28 -0600

    Actualizacion de directrises de calidad

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import BasicInput from './sites/BasicInput'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+function App() {
+
+  return (
+    <Router>
+      <div style={{paddingLeft: "30px"}}>
+      <h1>RegulonDB UI-Components</h1>
+      <Link to="/BasicInput"><h2>BasicInput</h2></Link>
+      <Link to="/InfoDisplay"><h2>InfoDisplay</h2></Link>
+      <Link to="/RegulonDB-webComponents"><h2>RegulonDB-webComponents</h2></Link>
+      
+      <Switch>
+          <Route exact path="/BasicInput">
+            <BasicInput />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+    
+  );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,12 @@
-import React, { Component } from 'react'
-import PropTypes from 'prop-types'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
 
-const W = 500
-const H = 500
+ReactDOM.render(<App />, document.getElementById('root'));
 
-export class Hola extends Component {
-  render() {
-    return (
-      <div >
-        <h1>Hola</h1>
-      </div>
-    )
-  }
-}
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
