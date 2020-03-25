d71ba4a6161e4681154a3d0c8e6f602ae13064cc
Galarcon128 <alarcongabriel128@gmail.com>
2020-03-25 10:14:56 -0600

    add header flip

--- src/ui-components/basicInput/buttons/IconButton.js
+++ src/ui-components/basicInput/buttons/IconButton.js
@@ -1,3 +1,102 @@
+/**
+# Component (user guide)
+
+# Component name 
+	
+	[IconButton]
+	
+## Description  
+	
+	[-]
+
+## Category   
+	
+	[Visual, Structural]  
+
+## Live demo 
+	
+	[code to iframe CodeSandbox]
+
+
+## Installation 
+
+	[example: npm install --save react-awesome-button]
+
+## Usage 
+	
+	[example: <protvista-tooltip>  </protvista-tooltip> ]
+
+## Props 
+
+  | Prop    | type     | default                   | description                                                  |
+| ------- | -------- | ------------------------- | ------------------------------------------------------------ |
+| active  | boolean  | true                      | enables or disables the button                               |
+| icon    | string   | "sentiment_satisfied_alt" | Shows an icon inside the button, you can get the name of the icon using the MATERIAL-ICONS library  https://material.io/resources/icons/?style=baseline |
+| style   | object   | {}                        |                                                              |
+| onClick | function |                           |                                                              |
+
+
+
+## Exception
+	__Category (Error, Warning or Message)__  
+	Description of the exception (if necessary)
+
+## License
+
+	[-]
+
+## Author 
+	
+	[CCG-UNAM-RegulonDB]
+
+**/
+
+
+/**
+# Component (technical guide)
+
+## Component Type 
+
+	[pure]
+
+## Dependencies
+
+	[React, { Component },PropTypes]
+
+## States
+	
+	| Property | Value | Desctiption |
+	| -------- | ----- | ----------- |
+	|          |       |             |
+	
+
+# Function description
+
+	## [noAction]
+
+	__Description:__  
+	[this function sends the console a warning that the button does not have a designated action]
+
+	__Usage:__
+
+		```javascript
+
+		noAction;
+
+		```
+	__Scope: __
+
+	[Scope details]
+
+	__Input Parameter:__  
+		* __Param - __ __[Value]__
+		Description
+
+	__Return:__  
+		* __Type - __ __[Name]__
+        Description (if necessary)
+**/
+
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import './Style.css';
