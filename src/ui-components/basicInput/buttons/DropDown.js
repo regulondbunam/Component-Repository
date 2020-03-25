c39bb3f3c845959db87e82c25ad751011cbfc6ae
Galarcon128 <alarcongabriel128@gmail.com>
2020-03-25 09:49:19 -0600

    dropdown add header

--- src/ui-components/basicInput/buttons/DropDown.js
+++ src/ui-components/basicInput/buttons/DropDown.js
@@ -1,3 +1,105 @@
+/**
+# Component (user guide)
+
+# Component name 
+	
+	[DropDown]
+	
+## Description  
+	
+	[The DropDown button allows the user an interactivity with several options in an agile way, this component when pressed displays a box with multiple options]
+
+## Category   
+	
+	[Visual]  
+
+## Live demo 
+	
+	[]
+
+
+## Installation 
+
+	[-]
+
+## Usage 
+	
+	[example: <DropDown content={options} onSelect={dropSelect}/> ]
+
+## Props 
+
+  | prop name   | Type     | Default                                                  | Description                                                  |
+| ----------- | -------- | -------------------------------------------------------- | ------------------------------------------------------------ |
+| active      | boolean  | true                                                     | enables or disables the button                               |
+| content     | array    | []                                                       | receives an array type variable containing the options to be displayed in the dropdown |
+| label       | string   | "DropDown"                                               | "dropdown label"                                             |
+| labelUpdate | boolean  | true                                                     | enables the update of the dropdown label when an option is selected |
+| style       | object   | {width: "auto", paddingLeft: "5px", paddingRight: "5px"} | style of dropdown                                            |
+| onSelect    | function | noAction(value)                                          | receives the function that will be executed when an option is selected, the function revives the 'value' parameter containing the selected array element |
+
+
+
+## Exception
+	__Category (Error, Warning or Message)__  
+	Description of the exception (if necessary)
+
+## License
+
+	[License details]
+
+## Author 
+	
+	[CCG-UNAM-RegulonDB]
+
+**/
+
+
+/**
+# Component (technical guide)
+
+## Component Type 
+
+	[stateless]
+
+## Dependencies
+
+	[React, { Component },PropTypes, RegulonDB-uicomponents Button]
+
+## States
+	
+	| Property | Value | Desctiption |
+	| -------- | ----- | ----------- |
+	|          |       |             |
+	
+
+# Function description
+
+    ## [noAction]
+
+	__Description:__  
+	[this function sends the console a warning that the button does not have a designated action]
+
+	__Usage:__
+
+		```javascript
+
+		noAction;
+
+		```
+	__Scope: __
+
+	[Scope details]
+
+	__Input Parameter:__  
+		* __Param - __ __[Value]__
+		Description
+
+	__Return:__  
+		* __Type - __ __[Name]__
+        Description (if necessary)
+
+**/
+
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import Button from './Button'
@@ -14,7 +116,7 @@ const arrowUp = "keyboard_arrow_up"
 const styleDropDown = {
     width: "auto",
     paddingLeft: "5px",
-    paddingRight: "5px",
+    paddingRight: "5px"
 }
 
 export default class DropDown extends Component {
