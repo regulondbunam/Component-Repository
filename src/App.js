89bff03ef0b0da7d4d2afba47b4496f50347e452
Gabriel Alarcon <34384736+Galarcon128@users.noreply.github.com>
2020-05-01 08:33:55 -0500

    init RegulonDB-Components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import BasicInput from './sites/BasicInput'
 import InfoDisplay from './sites/InfoDisplay'
+import RegulonDBWeb from './sites/RegulonDB-web'
 import {
   BrowserRouter as Router,
   Switch,
@@ -16,7 +17,7 @@ function App() {
         <h1>RegulonDB UI-Components</h1>
         <Link to="/BasicInput"><h2>BasicInput</h2></Link>
         <Link to="/InfoDisplay"><h2>InfoDisplay</h2></Link>
-        <Link to="/RegulonDB-webComponents"><h2>RegulonDB-webComponents</h2></Link>
+        <Link to="/RegulonDB-web"><h2>RegulonDB-web</h2></Link>
 
         <Switch>
           <Route exact path="/BasicInput">
@@ -24,6 +25,9 @@ function App() {
           </Route>
           <Route exact path="/InfoDisplay">
             <InfoDisplay />
+          </Route>
+          <Route exact path="/RegulonDB-web">
+            <RegulonDBWeb />
           </Route>
         </Switch>
       </div>
---
+++ src/sites/RegulonDB-web.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+
+export default function BasicInput() {
+
+  return (
+      <div>
+          <h2>Cover</h2>
+          <br/>
+          <br/>
+      </div>
+  )
+
+}
