6f5b29cf50a87a9bf8e4f879daa796c6a3244a45
Gabriel Alarcon <34384736+Galarcon128@users.noreply.github.com>
2020-05-05 14:20:21 -0500

    add cover component funcionality img and video

--- src/ui-components/regulonDB-web/cover/Cover.js
+++ src/ui-components/regulonDB-web/cover/Cover.js
@@ -1,26 +1,48 @@
 import React, { Component } from 'react';
 import CoverVideo from './CoverVideo'
+import CoverImg from './CoverImg'
 import './Style.css'
+
+const testimg = 'https://wallpapertag.com/wallpaper/full/a/4/1/148303-gorgerous-dna-background-1920x1080-for-mac.jpg'
 
 export default class Cover extends Component {
 
   render() {
 
     const {
-      imgUrl,
+      coverType,
       links,
-      videoUrl,
+      source,
+      opacity,
       title,
     } = this.props
+
+    let displayed
+
+    switch(coverType){
+      case 'video':
+        displayed = Video(source)
+        break
+      case 'img':
+      case 'image':
+      case 'imagen':
+        displayed = Image(source)
+        break
+      default:
+        displayed = Color(source)
+              
+    }
     
     return (
       <div className="coverComponent">
-        <div className="coverComponent-Glass" />
+        <div className="coverComponent-Glass" style={{opacity: opacity}} />
         <div className="coverComponent-content">
           <h1>{title}</h1>
         </div>
         <div className="coverComponent-background">
-          <CoverVideo url={videoUrl}/>
+          {
+            displayed
+          }
         </div>
       </div>
 
@@ -31,13 +53,27 @@ export default class Cover extends Component {
 
 }
 
-function CheckURL(url){
 
+function Image(url){
+  return(
+    <CoverImg urlImage={url}/>
+  )
+}
+
+function Color(color){
+  console.log(color)
+}
+
+function Video(videoUrl){
+  return (
+    <CoverVideo url={videoUrl}/>
+  )
 }
 
 Cover.defaultProps = {
-  imgUrl: [],
+  coverType: "Color",
   links: [],
-  videoUrl: "",
+  opacity: 0.6,
+  source: '#000000',
   title: "Cover Title",
 }
