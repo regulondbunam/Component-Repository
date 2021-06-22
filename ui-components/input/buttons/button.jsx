2ec38029754c81903c23456109177d2cccf14971
Gabriel CCG <galarcon@ccg.unam.mx>
2021-06-22 09:27:22 -0500

    add iframe

--- ui-components/input/buttons/button.jsx
+++ ui-components/input/buttons/button.jsx
@@ -8,7 +8,14 @@
 ## Category   
 [Visual, Functional Component]  
 ## Live demo 
-[-]
+
+<iframe src="https://codesandbox.io/embed/awesome-breeze-20lr3?fontsize=14&hidenavigation=1&theme=dark"
+     style="width:100%; height:500px; border:0; border-radius: 4px; overflow:hidden;"
+     title="awesome-breeze-20lr3"
+     allow="accelerometer; ambient-light-sensor; camera; encrypted-media; geolocation; gyroscope; hid; microphone; midi; payment; usb; vr; xr-spatial-tracking"
+     sandbox="allow-forms allow-modals allow-popups allow-presentation allow-same-origin allow-scripts"
+   ></iframe>
+
 ## Installation 
 [-]
 ## Usage ∫
